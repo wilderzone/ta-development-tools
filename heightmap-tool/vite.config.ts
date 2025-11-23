e30b5890f33cb52a9bb3f78377ba883587a5f53c
Giga <gigabyte5671@gmail.com>
2025-11-23 20:55:39 +1300

    Create `src` directory alias

--- heightmap-tool/vite.config.ts
+++ heightmap-tool/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -29,4 +30,9 @@ export default defineConfig(async () => ({
 			ignored: ['**/src-tauri/**'],
 		},
 	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 }));
